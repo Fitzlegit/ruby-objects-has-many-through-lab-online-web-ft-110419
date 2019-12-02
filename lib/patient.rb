4a359790816c0e7096e711499e318b81b4c6ce98
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-02 13:08:58 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+class Patient
+  attr_accessor :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def new_appointment
+  end
+
+  def appointments
+  end
+
+  def doctors
+  end
+
+
+end
