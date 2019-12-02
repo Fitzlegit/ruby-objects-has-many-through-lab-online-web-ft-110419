591e47683ec4fc5311f01307ecd336a0e544b8ab
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-02 10:51:10 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select{ |song| song.artist == self }
+  end
+
+  def genres
+    Genre.all.select{|genre| genre.artist == self}
+  end
+
+end
